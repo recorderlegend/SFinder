11fb667334e89d1159956fa974c15383ffa845cd
recorderlegend <abdolla.hegazy2006@gmail.com>
2024-08-09 23:42:55 -0400

    fixes

--- current/static/script.js
+++ current/static/script.js
@@ -1,5 +1,3 @@
-const { update, set } = require("firebase/database");
-
 var tickers = JSON.parse(localStorage.getItem('tickers')) || [];
 var lastPrices = {};
 var counter = 15
@@ -17,23 +15,26 @@ function startUpdateCycle() {
 }
 
 $(document).ready(function () {
-    tickers.forEach(function (tickers) {
-        addTickerToGrid();
+    tickers.forEach(function (ticker) {
+        addTickerToGrid(ticker);
     });
+
+    updatePrices();
+
     $('#add-ticker-form').submit(function (e) {
-        e.preventDefualt();
+        e.preventDefault();
         var newTicker = $('#new-ticker').val().toUpperCase();
         if (!tickers.includes(newTicker)) {
             tickers.push(newTicker)
             localStorage.setItem('tickers', JSON.stringify(tickers))
             addTickerToGrid(newTicker)
         }
-        $('new-ticker').val('');
+        $('#new-ticker').val('');
         updatePrices();
     })
     $('#tickers-grid').on('click', '.remove-btn', function () {
         var tickerToRemove = $(this).data('ticker')
-        tickers = ticker.filter(t => t !== tickerToRemove)
+        tickers = tickers.filter(t => t !== tickerToRemove)
         localStorage.setItem('tickers', JSON.stringify(tickers))
         $(`#${tickerToRemove}`).remove()
     })
@@ -42,12 +43,12 @@ $(document).ready(function () {
 
 
 function addTickerToGrid(ticker) {
-    $('#ticker-grid').append(`<div id="${ticker}" class = "stock-box"><h2>${ticker}</h2><p id ="${ticker}--price"></p><p id="${ticker}--pct"></p><button class ="remove-btn" data-ticker="${ticker}">Remove</button></div>`)
+    $('#tickers-grid').append(`<div id="${ticker}" class="stock-box"><h2>${ticker}</h2><p id="${ticker}--price"></p><p id="${ticker}--pct"></p><button class="remove-btn" data-ticker="${ticker}">Remove</button></div>`)
 
 }
 
 
-function updatePrice() {
+function updatePrices() {
     tickers.forEach(function (ticker) {
         $.ajax({
             url: '/get_stock_data',
@@ -55,16 +56,16 @@ function updatePrice() {
             data: JSON.stringify({ 'ticker': ticker }),
             contentType: 'application/json; charset=utf-8',
             dataType: 'json',
-            sucess: function (data) {
-                var changePercent = ((data.currentPrice - data.openPrice) / data.openPrice) * 100;
+            success: function (data) {
+                var changepercent = ((data.currentPrice - data.openPrice) / data.openPrice) * 100;
                 var colorClass;
                 if (changepercent <= -2) {
                     colorClass = 'dark-red'
                 } else if (changepercent < 0) {
                     colorClass = 'red'
-                } else if (changePercent == 0) {
+                } else if (changepercent == 0) {
                     colorClass = 'gray'
-                } else if (changePercent <= 2) {
+                } else if (changepercent <= 2) {
                     colorClass = 'green'
                 } else {
                     colorClass = 'dark-green'
@@ -78,7 +79,7 @@ function updatePrice() {
                 var flashClass
                 if (lastPrices[ticker] > data.currentPrice) {
                     flashClass = 'red-flash'
-                } else if (lastPrices[ticker] > data.currentPrice) {
+                } else if (lastPrices[ticker] < data.currentPrice) {
                     flashClass = 'green-flash'
                 } else {
                     flashClass = 'gray-flash'
