f17ad640d08f7a17d2587369d5f1ad531f11391e
recorderlegend <abdolla.hegazy2006@gmail.com>
2024-08-10 19:22:33 -0400

    reactset

---
+++ reactTest/App.js
@@ -0,0 +1,24 @@
+import Header from './Header'
+
+
+function App() {
+    return (
+        <div className="App">
+            {/* Header */}
+            <div className="app__header">
+            <Header>
+                    
+
+            </Header>
+            </div>
+            {/* Body*/}
+            <div class="app__body">
+
+
+            </div>
+
+
+        </div>
+    )
+
+}
---
+++ reactTest/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+      <div className="header__wrapper">
+          {/* logo */}
+          <div classNam="header__logo"></div>
+          {/* search */}
+          {/* menuitems */}
+          
+
+    </div>
+  )
+}
+
+export default Header
